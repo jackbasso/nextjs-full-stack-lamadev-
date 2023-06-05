8f5ce7c154e126db7c40e53b990090a98e8ff363
jackbasso <jackbasso.r@gmail.com>
2023-06-04 23:07:30 -0600

    About page v1 completed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
   images: {
-    domains: [
-      "images.pexels.com",
-      "example.com",
-    ]
-  }
-
+    domains: ["images.pexels.com"],
+  },
+};
 
 module.exports = nextConfig
