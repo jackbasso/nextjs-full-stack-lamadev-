4c5bd364a76fc9bb76a50259c8807d2c1c677719
jackbasso <jackbasso.r@gmail.com>
2023-06-10 18:39:44 -0600

    Single post v1 completed

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -5,10 +5,10 @@ import { notFound } from "next/navigation";
 
 async function getData(id) {
   //const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    const res = await fetch(`http://localhost:3000/api/posts/${id}`)
-  //   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-  //   cache: "no-store",
-  // });
+   // const res = await fetch(`http://localhost:3000/api/posts/${id}`)
+    const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     return notFound()
